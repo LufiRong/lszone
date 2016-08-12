4a16bcae22400a7b636a31552d754389a4f84be7
rong <476935846@qq.com>
2016-08-12 11:28:35 +0800

    no message

--- js/index.js
+++ js/index.js
@@ -1,4 +1,12 @@
-
+//初始化
+var init = function(){
+	//页面自适应
+	var screenWidth = $('body').width();
+	var fontS = screenWidth/750*100;
+	var html = document.getElementsByTagName('html')[0];
+	html.style.fontSize = fontS+'px';
+};
+init();
 //微信显示开关
 $('#weixin').on('click',function(){
 	$('.weixinbox').toggle();
